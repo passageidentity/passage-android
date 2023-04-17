60e317811553b55a191588f27eaded07d1952c65
Ricky Padilla <rickycpadilla@gmail.com>
2023-04-17 10:31:21 -0600

    Initial commit

---
+++ passage/src/main/java/id/passage/android/PassageAuth.kt
@@ -0,0 +1,14 @@
+package id.passage.android
+
+import android.util.Log
+
+class PassageAuth {
+
+    companion object {
+        private const val TAG = "PassageAuth"
+        fun helloPassage() {
+            Log.d(TAG, "Hello Passage!!!!!!!!!!!!!")
+        }
+    }
+
+}
