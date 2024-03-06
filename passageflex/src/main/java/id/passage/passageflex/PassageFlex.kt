b5f80dd4e259ba2ad1b1797bd1744493507ef14f
Ricky Padilla <rickycpadilla@gmail.com>
2024-03-06 14:13:46 -0700

    feat(PSG-3677): added Passage Flex module

---
+++ passageflex/src/main/java/id/passage/passageflex/PassageFlex.kt
@@ -0,0 +1,21 @@
+package id.passage.passageflex
+
+public object PassageFlex {
+
+    public object Passkey {
+
+        public suspend fun register() {
+
+        }
+
+        public suspend fun authenticate() {
+
+        }
+
+        public suspend fun autoFill() {
+
+        }
+
+    }
+
+}
