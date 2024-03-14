8924f2ccec0dae1660e966c0406429c85f7c856c
Ricky Padilla <rickycpadilla@gmail.com>
2024-03-14 12:58:13 -0600

    feat(PSG-3569): Added custom exceptions.

--- passageflex/src/main/java/id/passage/passageflex/PassageFlex.kt
+++ passageflex/src/main/java/id/passage/passageflex/PassageFlex.kt
@@ -9,48 +9,120 @@ import id.passage.android.passageflex.model.AuthenticatorAttachment
 import id.passage.android.passageflex.model.RegisterWebAuthnFinishWithTransactionRequest
 import id.passage.android.passageflex.model.RegisterWebAuthnStartWithTransactionRequest
 
+/**
+ *  Use `PassageFlex` to easily add passkeys to your existing authentication system on Apple devices.
+ *
+ *  Find out more at: https://passage.1password.com
+ */
 public object PassageFlex {
-
+    /**
+     * The base struct for utilizing Apple's native passkey APIs and Passage Flex APIs together.
+     */
     public object Passkey {
 
-        public suspend fun register(transactionId: String, activity: Activity, authenticatorAttachment: AuthenticatorAttachment? = null) : String {
-            val appId = "" // TODO: Get from utility method
-            val registerAPI = RegisterAPI()
-            val startRequest = RegisterWebAuthnStartWithTransactionRequest(transactionId, authenticatorAttachment)
-            val starResponse = registerAPI.registerWebauthnStartWithTransaction(appId, startRequest)
-            val createCredOptionsJson = PasskeyUtils.getCreateCredentialOptionsJson(starResponse.handshake)
-            val createCredResponse = PasskeyUtils.createPasskey(createCredOptionsJson, activity)
-            // Complete registration and authenticate the user
-            val handshakeResponse = PasskeyUtils.getCreateCredentialHandshakeResponse(createCredResponse)
-            val finishRequest = RegisterWebAuthnFinishWithTransactionRequest(
-                handshakeId = starResponse.handshake.id,
-                handshakeResponse,
-                transactionId
-            )
-            val finishResponse = registerAPI.registerWebauthnFinishWithTransaction(appId, finishRequest)
-            return finishResponse.nonce
+        /**
+         * Registers a new passkey.
+         *
+         * Prompts the user associated with the provided Passage `transactionId` to create and
+         * register a new passkey for use with your app.
+         *
+         * @param transactionId The Passage transaction id provided by your app's server.
+         * @param activity Activity to surface the Credentials Manager prompt within.
+         * @param authenticatorAttachment (Optional) The type of authentication that will be used in
+         * this WebAuthN flow request. Defaults to `AuthenticatorAttachment.platform`. Use
+         * `AuthenticatorAttachment.cross-platform` for physical security key registration.
+         * @return A single-use "nonce" from Passage server to be exchanged for an authentication
+         * token on your app's server.
+         * @throws RegisterException
+         */
+        public suspend fun register(
+            transactionId: String,
+            activity: Activity,
+            authenticatorAttachment: AuthenticatorAttachment = AuthenticatorAttachment.platform
+        ) : String {
+            try {
+                // Get Passage App ID from developer's `strings.xml` resource.
+                val appId = Utils.getAppId(activity)
+                // Request a Registration Start Handshake from Passage server
+                val registerAPI = RegisterAPI()
+                val startRequest = RegisterWebAuthnStartWithTransactionRequest(
+                    transactionId,
+                    authenticatorAttachment
+                )
+                val startResponse = registerAPI
+                    .registerWebauthnStartWithTransaction(
+                        appId,
+                        startRequest
+                    )
+                // Use the Registration Start Handshake to prompt the app user to create a passkey
+                val createCredOptionsJson = PasskeyUtils
+                    .getCreateCredentialOptionsJson(startResponse.handshake)
+                val createCredResponse = PasskeyUtils.createPasskey(createCredOptionsJson, activity)
+                val handshakeResponse = PasskeyUtils
+                    .getCreateCredentialHandshakeResponse(createCredResponse)
+                // Send the new Credential Handshake Response to Passage server
+                val finishRequest = RegisterWebAuthnFinishWithTransactionRequest(
+                    handshakeId = startResponse.handshake.id,
+                    handshakeResponse,
+                    transactionId
+                )
+                val finishResponse = registerAPI
+                    .registerWebauthnFinishWithTransaction(
+                        appId,
+                        finishRequest
+                    )
+                // If successful, Passage server will return a nonce.
+                return finishResponse.nonce
+            } catch (e: Exception) {
+                throw RegisterException.convert(e)
+            }
         }
 
+        /**
+         * Authenticates with a passkey.
+         *
+         * Prompts the user to select a passkey for authentication for your app. If a Passage
+         * `transactionId` is provided, this method will attempt to show only passkeys associated
+         * with that user account.
+         *
+         * @param transactionId (Optional) The Passage transaction id provided by your app's server.
+         * @param activity Activity to surface the Credentials Manager prompt within.
+         * @return A single-use "nonce" from Passage server to be exchanged for an authentication
+         * token on your app's server.
+         * @throws AuthenticateException
+         */
         public suspend fun authenticate(transactionId: String? = null, activity: Activity): String {
-            val appId = "" // TODO: Get from utility method
-            val authenticateAPI = AuthenticateAPI()
-            val startRequest = AuthenticateWebAuthnStartWithTransactionRequest(transactionId)
-            val startResponse = authenticateAPI.authenticateWebauthnStartWithTransaction(appId, startRequest)
-            val credOptionsJson = PasskeyUtils.getCredentialOptionsJson(startResponse.handshake)
-            val credResponse = PasskeyUtils.getPasskey(credOptionsJson, activity)
-            // Complete login and authenticate the user
-            val handshakeResponse = PasskeyUtils.getCredentialHandshakeResponse(credResponse)
-            val finishRequest = AuthenticateWebAuthnFinishWithTransactionRequest(
-                handshakeId = startResponse.handshake.id,
-                handshakeResponse,
-                transactionId
-            )
-            val finishResponse = authenticateAPI.authenticateWebauthnFinishWithTransaction(appId, finishRequest)
-            return finishResponse.nonce
-        }
-
-        public suspend fun autoFill(activity: Activity) {
-            val appId = "" // TODO: Get from utility method
+            try {
+                // Get Passage App ID from developer's `strings.xml` resource.
+                val appId = Utils.getAppId(activity)
+                // Request an Assertion Start Handshake from Passage server
+                val authenticateAPI = AuthenticateAPI()
+                val startRequest = AuthenticateWebAuthnStartWithTransactionRequest(transactionId)
+                val startResponse = authenticateAPI
+                    .authenticateWebauthnStartWithTransaction(
+                        appId,
+                        startRequest
+                    )
+                // Use the Assertion Start Handshake to prompt the app user to select a passkey
+                val credOptionsJson = PasskeyUtils.getCredentialOptionsJson(startResponse.handshake)
+                val credResponse = PasskeyUtils.getPasskey(credOptionsJson, activity)
+                val handshakeResponse = PasskeyUtils.getCredentialHandshakeResponse(credResponse)
+                // Send the Credential Handshake Response to Passage server
+                val finishRequest = AuthenticateWebAuthnFinishWithTransactionRequest(
+                    handshakeId = startResponse.handshake.id,
+                    handshakeResponse,
+                    transactionId
+                )
+                val finishResponse = authenticateAPI
+                    .authenticateWebauthnFinishWithTransaction(
+                        appId,
+                        finishRequest
+                    )
+                // If successful, Passage server will return a nonce.
+                return finishResponse.nonce
+            } catch (e: Exception) {
+                throw AuthenticateException.convert(e)
+            }
         }
 
     }
---
+++ passageflex/src/main/java/id/passage/passageflex/RegisterException.kt
@@ -0,0 +1,114 @@
+package id.passage.passageflex
+
+import androidx.credentials.exceptions.*
+import id.passage.android.passageflex.model.Model400Code
+import id.passage.android.passageflex.model.Model401Code
+import id.passage.passageflex.client.infrastructure.ClientException
+import id.passage.passageflex.client.infrastructure.ServerException
+
+/**
+ * Thrown when there's an error registering a user with a passkey.
+ *
+ * @see RegisterCancellationException
+ * @see RegisterInterruptedException
+ * @see RegisterConfigurationException
+ * @see RegisterNoCreateOptionException
+ * @see RegisterUnsupportedException
+ * @see RegisterInvalidRequestException
+ * @see RegisterCredentialException
+ * @see RegisterServerException
+ */
+public open class RegisterException(message: String) : PassageFlexException(message) {
+    internal companion object {
+        internal fun convert(e: Exception): RegisterException {
+            val message = e.message ?: e.toString()
+            return when (e) {
+                is CreateCredentialException -> convertCreateCredentialException(e)
+                is ClientException -> convertClientException(e)
+                is ServerException -> RegisterServerException(message)
+                else -> RegisterException(message)
+            }
+        }
+
+        private fun convertClientException(e: ClientException): RegisterException {
+            val error = parseClientException(e)
+            val message = error?.error ?: ""
+            return when (error?.code) {
+                Model400Code.request.toString() -> {
+                    RegisterInvalidRequestException(message)
+                }
+                Model401Code.webauthnLoginFailed.toString() -> {
+                    RegisterCredentialException(message)
+                }
+                else -> {
+                    RegisterException(message)
+                }
+            }
+        }
+
+        private fun convertCreateCredentialException(e: CreateCredentialException): RegisterException {
+            val message = e.message ?: e.toString()
+            return when (e) {
+                is CreateCredentialCancellationException -> {
+                    RegisterCancellationException(message)
+                }
+                is CreateCredentialInterruptedException -> {
+                    RegisterInterruptedException(message)
+                }
+                is CreateCredentialProviderConfigurationException -> {
+                    RegisterConfigurationException(message)
+                }
+                is CreateCredentialNoCreateOptionException -> {
+                    RegisterNoCreateOptionException(message)
+                }
+                is CreateCredentialUnsupportedException -> {
+                    RegisterUnsupportedException(message)
+                }
+                else -> RegisterException(message)
+            }
+        }
+    }
+}
+
+/**
+ * The user intentionally canceled the operation and chose not to register the credential.
+ */
+public class RegisterCancellationException(message: String) : RegisterException(message)
+
+/**
+ * Credential creation was interrupted. Consider retrying the call.
+ */
+public class RegisterInterruptedException(message: String) : RegisterException(message)
+
+/**
+ * Your app is missing the provider configuration dependency.
+ * Most likely, your app has not been properly configured for Passage.
+ */
+public class RegisterConfigurationException(message: String) : RegisterException(message)
+
+/**
+ * During the create credential flow, this is thrown when no viable creation options were found for
+ * the given CreateCredentialRequest.
+ */
+public class RegisterNoCreateOptionException(message: String) : RegisterException(message)
+
+/**
+ * During the create credential flow, this is thrown when credential manager is unsupported,
+ * typically because the device has disabled it or did not ship with this feature enabled.
+ */
+public class RegisterUnsupportedException(message: String) : RegisterException(message)
+
+/**
+ * Thrown because of a bad request, typically when an invalid transaction idz is provided.
+ */
+public class RegisterInvalidRequestException(message: String) : RegisterException(message)
+
+/**
+ * Thrown because of an issue with the passkey credential.
+ */
+public class RegisterCredentialException(message: String) : RegisterException(message)
+
+/**
+ * Thrown when Passage internal server error occurs.
+ */
+public class RegisterServerException(message: String) : RegisterException(message)
