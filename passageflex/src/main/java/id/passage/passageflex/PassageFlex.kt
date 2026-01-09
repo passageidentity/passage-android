ca8c72be3ac9c4380f2b5983b55c083ecbdf2923
Ricky Padilla <rickycpadilla@gmail.com>
2026-01-09 10:41:57 -0700

    chore: deprecate packages (#91)

--- passageflex/src/main/java/id/passage/passageflex/PassageFlex.kt
+++ passageflex/src/main/java/id/passage/passageflex/PassageFlex.kt
@@ -7,6 +7,10 @@ import android.app.Activity
  *
  *  Find out more at: https://passage.1password.com
  */
+@Deprecated(
+  message = "This SDK is no longer supported.",
+  level = DeprecationLevel.WARNING
+)
 public class PassageFlex(
     activity: Activity,
     appId: String,
